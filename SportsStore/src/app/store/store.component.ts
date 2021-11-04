46504a42f19c733ea02515b8a8f6eafb6e7a0a13
Mark <mranda10@my.centennialcollege.ca>
2021-11-04 12:33:04 -0400

    Add page separtion functionality and setup for cart model

--- SportsStore/src/app/store/store.component.ts
+++ SportsStore/src/app/store/store.component.ts
@@ -9,15 +9,57 @@ import { ProductRepository } from "../model/product.repository";
 
 export class StoreComponent
 {
+    public selectedCategory = '';
+    public productsPerPage = 4;
+    public selectedPage = 1;
+    public pageCount = 1;
+
     constructor(private repository: ProductRepository) { }
 
     get products(): Product[] 
     {
-        return this.repository.getProducts();
+        const pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+        return this.repository.getProducts(this.selectedCategory).slice(pageIndex, pageIndex + this.productsPerPage);
     }
 
     get categories(): string[] 
     {
         return this.repository.getCategories();
+    }
+
+    changeCategory(newCategory?: string): void 
+    {
+        this.selectedCategory = (newCategory !== null) ? newCategory! : '';
+    }
+
+    changePage(newPage: number): void
+    {
+        this.selectedPage = newPage;
+    }
+
+    changePageSize(newSize: number): void 
+    {
+        if (newSize === null || newSize === undefined) {
+            this.productsPerPage = 3;
+            this.changePage(1);
+        }
+        else {
+            this.productsPerPage = newSize;
+            this.changePage(1);            
+        }        
+    }
+
+    getPageCount() 
+    {
+        return Array(Math.ceil(this.repository
+            .getProducts(this.selectedCategory).length / this.productsPerPage))
+            .fill(0).map((x, i) => i + 1).length
+    }
+
+    get pageNumbers(): number[] 
+    {
+        return Array(Math.ceil(this.repository
+            .getProducts(this.selectedCategory).length / this.productsPerPage))
+            .fill(0).map((x, i) => i + 1);
     }
 }
--- SportsStore/src/app/store/store.module.ts
+++ SportsStore/src/app/store/store.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { ModelModule } from "../model/model.module";
 import { StoreComponent } from "./store.component";
+import { CounterDirective } from "./counter.directive";
+
 
 @NgModule({
     imports: [ModelModule, BrowserModule, FormsModule],
-    declarations: [StoreComponent],
-    exports: [StoreComponent]
+    declarations: [StoreComponent, CounterDirective],
+    exports: [StoreComponent, CounterDirective]
 })
 
 export class StoreModule { }
